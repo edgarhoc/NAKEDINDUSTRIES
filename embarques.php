f813085a760dddb1384f84c72186e49927dd8154
edgarhoc <58460348+edgarhoc@users.noreply.github.com>
2019-12-04 12:21:50 -0600

    Create embarques.php

---
+++ embarques.php
@@ -0,0 +1,45 @@
+<?php $conexion=mysqli_connect('localhost','root','','polizona_mercado');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Embarques</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/fontello.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+</head>
+	<body>
+		<br>
+		<div style="text-align:center;">
+			<table class="tablas" style="margin: 0 auto;">
+				<tr>
+					<td>Boleta</td>
+					<td>ID Embarque</td>
+					<td>ID Insumo</td>
+					<td>Unidades</td>
+					<td>Costo Unitario</td>	
+				</tr>
+
+				<?php 
+				$sql="SELECT * from embarques";
+				$result=mysqli_query($conexion,$sql);
+				while($mostrar=mysqli_fetch_array($result)){
+				?>
+
+				<tr>
+					<td><?php echo $mostrar['boleta'] ?></td>
+					<td><?php echo $mostrar['idembarque'] ?></td>
+					<td><?php echo $mostrar['idinsumo'] ?></td>
+					<td><?php echo $mostrar['unidades'] ?></td>
+					<td><?php echo $mostrar['costoUnitario'] ?></td>
+				</tr>
+				<?php 
+				}
+				 ?>
+			</table>
+		</div>
+	</body>
+</html>
